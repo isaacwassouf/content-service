847c3df52f5bc8ee4a7179dcc50768d9469cae45
Isaac Wassouf <isaacwassouf@gmail.com>
2024-06-23 11:34:47 +0200

    feat: init commit; add rpc method for getting an entity

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	db "github.com/isaacwassouf/content-service/database"
+	pb "github.com/isaacwassouf/content-service/protobufs/content_management_service"
+	"github.com/isaacwassouf/content-service/utils"
+)
+
+type ContentManagementService struct {
+	pb.UnimplementedContentServiceServer
+	contentManagementServiceDB *db.ContentManagementServiceDB
+}
+type Message struct {
+	Content string
+}
+
+func (s *ContentManagementService) GetContent(ctx context.Context, in *pb.GetContentRequest) (*pb.GetContentResponse, error) {
+	// check if the table exists
+	tableExists, err := utils.CheckTableExists(s.contentManagementServiceDB.Db, in.TableName)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Failed to check if table exists")
+	}
+	if !tableExists {
+		return nil, status.Error(codes.NotFound, "Table does not exist")
+	}
+
+	query := fmt.Sprintf("SELECT * FROM %s WHERE id = ? LIMIT 1", in.TableName)
+	rows, err := s.contentManagementServiceDB.Db.Query(query, in.EntityId)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Failed to query the database")
+	}
+
+	defer rows.Close()
+
+	cols, err := rows.Columns()
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Failed to get columns")
+	}
+
+	data := make(map[string]interface{}, len(cols))
+	columns := make([]interface{}, len(cols))
+	columnPointers := make([]interface{}, len(cols))
+
+	for i := range columns {
+		columnPointers[i] = &columns[i]
+	}
+
+	if rows.Next() {
+		_ = rows.Scan(columnPointers...)
+
+		for i, colName := range cols {
+			val := columnPointers[i].(*interface{})
+			// Check if the value is a byte slice and convert it to a string
+			if b, ok := (*val).([]byte); ok {
+				data[colName] = string(b)
+			} else {
+				data[colName] = *val
+			}
+		}
+	} else {
+		return nil, status.Error(codes.NotFound, "Entity not found")
+	}
+
+	// convert the data to bytes
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "Failed to marshal data")
+	}
+
+	return &pb.GetContentResponse{Content: dataBytes}, nil
+}
+
+func main() {
+	// load the environment variables from the .env file
+	err := utils.LoadEnvVarsFromFile()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// Create a new schemaManagementServiceDB
+	contentManagementServiceDB, err := db.NewContentManagementService()
+	if err != nil {
+		log.Fatalf("failed to create a new SchemaManagementServiceDB: %v", err)
+	}
+	// ping the database
+	err = contentManagementServiceDB.Db.Ping()
+	if err != nil {
+		log.Fatalf("failed to ping the database: %v", err)
+	}
+
+	// Start the server
+	ls, err := net.Listen("tcp", ":8085")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterContentServiceServer(s, &ContentManagementService{
+		contentManagementServiceDB: contentManagementServiceDB,
+	})
+
+	log.Printf("Server listening at %v", ls.Addr())
+
+	if err := s.Serve(ls); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- utils/helpers.go
+++ utils/helpers.go
@@ -1,1 +1,37 @@
 package utils
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvVarsFromFile() error {
+	err := godotenv.Load()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func CheckTableExists(db *sql.DB, tableName string) (bool, error) {
+	query := fmt.Sprintf("SHOW TABLES LIKE '%s'", tableName)
+	rows, err := db.Query(query)
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+
+	return rows.Next(), nil
+}
+
+func CheckColumnExists(db *sql.DB, tableName string, columnName string) (bool, error) {
+	query := fmt.Sprintf("SHOW COLUMNS FROM %s LIKE '%s'", tableName, columnName)
+	rows, err := db.Query(query)
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+
+	return rows.Next(), nil
+}
